0738c0ea94c4352e2d029d0d9063e2939c681c2f
aminaamin0211 <aminaamin0211@gmail.com>
2022-10-15 15:50:24 +0100

    Add more endpoints to app.py

--- app.py
+++ app.py
@@ -4,5 +4,36 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.get("/")
+def pokemon_list():
+    return "Charmander, pikachu, eevee, bulbasaur, diglett"
+
+
+@app.get("/bulbasaur")
+def bulbasaur_data():
+    return "This is bulbasaur, a generation 1 pokemon who looks like a little dinosaur"
+
+
+@app.get("/charmander")
+def charmander_data():
+    return "This is charmander, who looks like a little reptile"
+
+
+@app.get("/pikachu")
+def pikachu_data():
+    return "This is pikachu, who looks like a little rodent"
+
+
+@app.get("/eevee")
+def eevee_data():
+    return "This is eevee a generation 1 pokemon, who looks like a little fox"
+
+
+
+@app.get("/diglett")
+def diglett_data():
+    return "This is diglett, a generation 1 pokemon, who looks like a little mole."
+
+
 if __name__ == "__main__":
     app.run()
